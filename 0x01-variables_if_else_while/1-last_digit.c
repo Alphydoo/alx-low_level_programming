8ba41a6dc3bc3aea9c099f880e9acb6d5c496f23
Alphydoo <aaoshiole@yahoo.com>
2022-06-04 01:38:53 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,11 +19,12 @@ int main(void)
 		printf("greater than \n");
 	}
 	else if (n % 10 == 0)
+	{
 		printf("0\n");
         }
         else
         {
-	printf("less than 6 and not 0\n");
+        	printf("less than 6 and not 0\n");
         }
         return (0);
 }
