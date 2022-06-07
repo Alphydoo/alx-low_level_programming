764df5182318d1d23ebf199f67e8e2b094bbb4a4
Alphydoo <aaoshiole@yahoo.com>
2022-06-07 00:30:48 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	_putchar('m');
+	_putchar('a');
+	_putchar('i');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
