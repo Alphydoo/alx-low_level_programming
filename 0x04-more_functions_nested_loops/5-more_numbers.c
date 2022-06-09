132cd741a295e5a0fe67e0a825e12e1c72f2aec3
Alphydoo <aaoshiole@yahoo.com>
2022-06-09 14:01:03 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,16 @@ void more_numbers(void)
 {
 	int i, xo;
 
-	for (xo = 0; i <= 14; xo++)
+	for (xo = 0; xo < 10; xo++)
 	{
-		if (i >= 10)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar((i / 10) + '0');
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
 		}
-		_putchar((i % 10) + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
