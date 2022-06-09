575c09449b7e44c81015300af87979a384e01296
Alphydoo <aaoshiole@yahoo.com>
2022-06-09 05:25:15 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - a function that multiples two intergers
+ * @a: first input
+ * @b: second input
+ * Return: multiplication result of the e inputs
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
