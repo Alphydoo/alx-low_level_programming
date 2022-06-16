d90777130cb9dcec8e870eabfd53ba115aba861f
Alphydoo <aaoshiole@yahoo.com>
2022-06-16 09:53:18 +0100

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,22 @@
 #include "main.h"
-
 /**
- * cap_string - a function that capitalizes all words of a string
- * @n: input string
- * Return: caps on first letter of a separator
+ * cap_string - capitalizes all words of a string.
+ * @s: pointer to input string.
+ * Return: Returns pointer to capitalized string.
  */
 char *cap_string(char *n)
 {
-	int i, x;
-	int cap = 32;
-	int separators[] = {',', ';', '.', '!','?', '"',
-		 '(', ')', '{', '}', '\n', '\t'};
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; n[i] != '\0'; i++)
+	i = 1;
+	if (n[0] >= 'a' && n[0] <= 'z')
+		n[0] -= ('a' - 'A');
+	while (n[i] != '\0')
 	{
-		if (n[i] >= 'a' && n[i] <= 'z')
-		{
-			n[i] = n[i] - cap;
-		}
-
-		cap = 0;
-
-		for (x = 0; x <= 12; x++)
-		{
-			if (n[i] == separators[x])
-			{
-				x = 12;
-				cap = 32;
-			}
-		}
+		for (j = 0; sep[j] != '\0'; j++)
+			if (n[i - 1] == sep[j] && (n[i] >= 'a' && n[i] <= 'z'))
+				n[i] -= ('a' - 'A');
+		i++;
 	}
 	return (n);
-}
