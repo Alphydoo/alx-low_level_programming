df52297bf75eb585d9d5af6d09f7787c1cc35268
Alphydoo <aaoshiole@yahoo.com>
2022-07-25 13:58:19 +0100

    Header Files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HEADER_H
+#define HEADER_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
-int _putchar(char c);
+#include <fcntl.h>
+#define BUFSIZE 1204
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
 
-#endif /* MAIN_H */
+#endif 
